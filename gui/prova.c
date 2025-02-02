4d164c00e27076db8229bba216efeaaf4b0433e1
Haglard <sandro_borioni@yahoo.com>
2025-02-02 16:13:37 +0100

    versione di oggi

---
+++ gui/prova.c
@@ -0,0 +1,14 @@
+chess_gui.c
+chess_gui_font.c
+chess_gui_font.h
+chess_gui.h
+chess_to_gui_interface.c
+chess_to_gui_interface.h
+compila.sh
+esegui.sh
+esempio
+esempio.c
+esempio_font.c
+esempio_mock_up.c
+images
+prova.c
